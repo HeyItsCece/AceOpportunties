11e3b8f6b3754dac4c56f72edf7f56141f8f54cc
Girls Who Code <GWC@cs81-ms-crt-13.local>
2019-07-15 15:12:35 -0400

    Created OpportunitiesViewController to control opportunities view, added outlet for the tableview on the opportunities view to the OpportunitiesViewController.

---
+++ AceOpportunties/OpportunitiesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OpportunitiesViewController.swift
+//  AceOpportunties
+//
+//  Created by Girls Who Code on 7/15/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class OpportunitiesViewController: UIViewController {
+
+    @IBOutlet weak var OpportunitiesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
