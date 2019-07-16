681c9173d71d7222778a87d9ed1bc104b70bebb1
Girls Who Code <GWC@cs81-ms-crt-13.local>
2019-07-16 13:25:00 -0400

    created high school opportunities view controller and created tableview outlet

---
+++ AceOpportunties/HSOpportunitiesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HSOpportunitiesViewController.swift
+//  AceOpportunties
+//
+//  Created by Girls Who Code on 7/16/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class HSOpportunitiesViewController: UIViewController {
+
+    @IBOutlet weak var hsOpportunitiesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
