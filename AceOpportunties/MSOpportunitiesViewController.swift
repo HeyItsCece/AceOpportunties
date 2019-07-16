ec1038315b3b16a1e3dc939709420d724280d540
Girls Who Code <GWC@cs81-ms-crt-13.local>
2019-07-16 14:03:09 -0400

    modified constraints for HS opportunities view, added college opportunities view from Cece, added content to cells for MS opportunities view (not working yet). Also added segways from the buttons to these views.

--- AceOpportunties/MSOpportunitiesViewController.swift
+++ AceOpportunties/MSOpportunitiesViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class MSOpportunitiesViewController: UIViewController {
-
+class MSOpportunitiesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    /////////////// OUTLETS ////////////////
     @IBOutlet weak var msOpportunitiesTableView: UITableView!
     
+    ///////// STRUCTS & CLASSES ////////////
+    struct Opportunity {
+        var title: String
+        var details: String
+    }
+    
+    ////////////// VARIABLES ///////////////
+    
+    var msOpportunities: [Opportunity] = [Opportunity(title:"Girls Who Code Campus", details:"The Girls Who Code campus program is a two week program which introduces girls from ages  10- 18 to computer science. It offers various courses such as: Intro to computer science, Iphone App development 1,Iphone App development 2,and Web Design. It is held in cities like NYC, los Angeles,Seattle,Boston, Bay Area, and more. Aside from this, Girls Who Code offers discounts and need based scholarships!")]
+    
+    //////////// FUNCTIONS /////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.msOpportunitiesTableView.rowHeight = UITableView.automaticDimension
         // Do any additional setup after loading the view.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return msOpportunities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = msOpportunitiesTableView.dequeueReusableCell(withIdentifier: "msCell", for: indexPath)
+        cell.textLabel?.text = msOpportunities[indexPath.row].title
+        cell.detailTextLabel?.text = msOpportunities[indexPath.row].details
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
