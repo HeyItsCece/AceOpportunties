235f68be624651b794f32aff2d2fb716a3e60e36
Girls Who Code <GWC@cs81-ms-crt-13.local>
2019-07-16 15:46:45 -0400

    information is still not showing up in table, fix it tomorrow!

--- AceOpportunties/MSOpportunitiesViewController.swift
+++ AceOpportunties/MSOpportunitiesViewController.swift
@@ -14,20 +14,32 @@ class MSOpportunitiesViewController: UIViewController, UITableViewDataSource, UI
     @IBOutlet weak var msOpportunitiesTableView: UITableView!
     
     ///////// STRUCTS & CLASSES ////////////
-    struct Opportunity {
-        var title: String
-        var details: String
-    }
     
     ////////////// VARIABLES ///////////////
-    
-    var msOpportunities: [Opportunity] = [Opportunity(title:"Girls Who Code Campus", details:"The Girls Who Code campus program is a two week program which introduces girls from ages  10- 18 to computer science. It offers various courses such as: Intro to computer science, Iphone App development 1,Iphone App development 2,and Web Design. It is held in cities like NYC, los Angeles,Seattle,Boston, Bay Area, and more. Aside from this, Girls Who Code offers discounts and need based scholarships!")]
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var msOpportunities: [Opportunity] = []
     
     //////////// FUNCTIONS /////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.msOpportunitiesTableView.rowHeight = UITableView.automaticDimension
         // Do any additional setup after loading the view.
+        let test: Opportunity = Opportunity(context: context)
+        test.title = "testing"
+        test.details = "details about testing"
+        test.ages = "MS"
+        getData()
+    }
+    
+    func getData() {
+        do {
+            msOpportunities = try context.fetch(Opportunity.fetchRequest())
+            print(msOpportunities)
+            DispatchQueue.main.async {
+                self.msOpportunitiesTableView.reloadData()
+            }
+        } catch {
+            print("Couldn't fetch data")
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -35,10 +47,14 @@ class MSOpportunitiesViewController: UIViewController, UITableViewDataSource, UI
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = msOpportunitiesTableView.dequeueReusableCell(withIdentifier: "msCell", for: indexPath)
-        cell.textLabel?.text = msOpportunities[indexPath.row].title
-        cell.detailTextLabel?.text = msOpportunities[indexPath.row].details
+        let cell = msOpportunitiesTableView.dequeueReusableCell(withIdentifier: "msCell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "\(msOpportunities[indexPath.row].title ?? "n/a") \(msOpportunities[indexPath.row].details ?? "n/a")"
+        print("\(msOpportunities[indexPath.row].title ?? "n/a") \(msOpportunities[indexPath.row].details ?? "n/a")")
         return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getData()
     }
     
     /*
