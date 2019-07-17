00438e943a20bc90d511a8683c197b69a669e075
Girls Who Code <GWC@cs81-ms-crt-13.local>
2019-07-17 09:33:06 -0400

    created list and college opportunity view controllers and connected them to the list and college opportunity views respectively

---
+++ AceOpportunties/CollegeOpportunitiesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollegeOpportunitiesViewController.swift
+//  AceOpportunties
+//
+//  Created by Girls Who Code on 7/17/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class CollegeOpportunitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
